b7da577d95a209684aae2bd4d4cf4b774d0e4914
reedmi <reedmi@163.com>
2014-08-19 19:16:36 +0800

    添加AbstractController

---
+++ src/main/java/com/reedmi/framework/web/controller/AbstractController.java
@@ -0,0 +1,69 @@
+package com.reedmi.framework.web.controller;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class AbstractController extends HandlerInterceptorAdapter {
+
+	private static final ThreadLocal<HttpServletRequest> REQUEST = new ThreadLocal<>();
+
+	private static final ThreadLocal<HttpServletResponse> RESPONSE = new ThreadLocal<>();
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		REQUEST.set(request);
+		RESPONSE.set(response);
+		return super.preHandle(request, response, handler);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+		REQUEST.remove();
+		RESPONSE.remove();
+		if (ex != null){
+			response.sendError(500);
+		}
+		super.afterCompletion(request, response, handler, ex);
+	}
+
+	/**
+	 * 获取当前请求中的HttpServletRequest实例。
+	 */
+	public HttpServletRequest request(){
+		return REQUEST.get();
+	}
+
+	/**
+	 * 获取当前请求中的HttpServletRequest实例。
+	 */
+	public HttpServletResponse response(){
+		return RESPONSE.get();
+	}
+
+	/**
+	 * 请求不合法的后续处理。
+	 */
+	public void badRequest(String info){
+		try {
+			response().sendError(500);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 向客户端反馈状态码。
+	 * @param status 200/500...
+	 */
+	public void sendStatus(int status){
+		response().setStatus(status);
+		try {
+			response().getOutputStream().close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
--- src/main/java/com/reedmi/framework/web/controller/HelloController.java
+++ src/main/java/com/reedmi/framework/web/controller/HelloController.java
@@ -1,14 +1,22 @@
 package com.reedmi.framework.web.controller;
 
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class HelloController {
+public class HelloController extends AbstractController {
 
-	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	@RequestMapping(value = "hello", method = RequestMethod.GET)
 	public String hello() {
+		for (Map.Entry<String, String[]> entry : (Set<Map.Entry<String, String[]>>)request().getParameterMap().entrySet()){
+			String key = entry.getKey();
+			String value = entry.getValue()[0];
+			System.out.println(key + "=" + value);
+		}
 		return "hello";
 	}
 }
